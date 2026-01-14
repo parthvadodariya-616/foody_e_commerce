463d28b9258fed9d9f7b2d101a9db0f372f13ec8
parthvadodariya-616 <parthvadodariya2005@gmail.com>
2026-01-15 02:25:45 +0530

    Initial Commit

---
+++ android/app/src/main/java/com/parth/io/task/foody_e_commerce/MainActivity.java
@@ -0,0 +1,7 @@
+package com.parth.io.task.foody_e_commerce;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterFragmentActivity;
+
+public class MainActivity extends FlutterFragmentActivity {
+}
