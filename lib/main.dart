9c45cc8ad882a5e571ea9466ad63b852d611a1c5
parthvadodariya-616 <parthvadodariya2005@gmail.com>
2026-01-15 02:29:31 +0530

    Add Firebase configuration and widget styling support

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:foody_e_commerce/Content/bottom_nav.dart';
+import 'package:foody_e_commerce/Content/onboard.dart';
+import 'package:foody_e_commerce/services/app_constraint.dart';
+import 'package:foody_e_commerce/services/local_cache.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  final LocalCacheService cacheService = LocalCacheService();
+  Stripe.publishableKey = publishableKey;
+  await cacheService.init();
+  runApp(MyApp(cacheService: cacheService));
+}
+
+class MyApp extends StatelessWidget {
+  final LocalCacheService cacheService;
+
+  const MyApp({Key? key, required this.cacheService}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Foody E-com',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        scaffoldBackgroundColor: Colors.grey[900],
+      ),
+      home: AuthWrapper(cacheService: cacheService),
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  final LocalCacheService cacheService;
+
+  const AuthWrapper({Key? key, required this.cacheService}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+        if (snapshot.hasData && snapshot.data != null) {
+          return BottomNav(); 
+        }
+        return Onboard(cacheService: cacheService); 
+      },
+    );
+  }
+}
